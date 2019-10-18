f359d6dc20970ee6d4845a6464cf0cebacf0535c
Ron <Lavitr@yandex.ru>
2019-10-18 17:19:51 +0300

    Example diagramm edded

--- src/containers/ItemDiagram.js
+++ src/containers/ItemDiagram.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import CanvasJSChart from '../canvasjs.react';
 import { connect } from 'react-redux'
 import { backToUsers } from '../actions'
 import data from '../Data'
@@ -11,20 +12,42 @@ const styleButton = {
     backgroundColor: 'orange'
 }
 
+class ItemDiagram extends React.Component {
 
-let ItemDiagram = ({ backToUsers, selectedItem }) => {
-    let selectedUserData
-    data.forEach(item => {
-        if (selectedItem === item.name) {
-            selectedUserData = item.data.a
+
+    render() {
+        let selectedUserData
+        data.forEach(item => {
+            if (this.props.selectedItem === item.name) {
+                selectedUserData = item.data.a
+            }
+        })
+        const options = {
+            title: {
+                text: "Basic Column Chart in React"
+            },
+            data: [{
+                type: "column",
+                dataPoints: [
+                    { label: "Apple", y: 10 },
+                    { label: "Orange", y: 15 },
+                    { label: "Banana", y: 25 },
+                    { label: "Mango", y: 30 },
+                    { label: "Grape", y: 28 }
+                ]
+            }]
         }
-    })
-    return (
-        <div >
-            <button style={styleButton} onClick={() => { backToUsers(); }} >BACK</button>
-            <div>{selectedUserData}</div>
-        </div>
-    )
+
+        return (
+            <div>
+                <CanvasJSChart options={options}
+                /* onRef = {ref => this.chart = ref} */
+                />
+                <button style={styleButton} onClick={() => { this.props.backToUsers(); }} >BACK</button>
+                <div>{selectedUserData}</div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => ({
